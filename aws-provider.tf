5d39dbb1c2e3c7dddba7785505b0d3ac822a7475
workstationbynick <139195581+workstationbynick@users.noreply.github.com>
2024-06-02 21:19:41 -0700

    aws-provider.tf

---
+++ aws-provider.tf
@@ -0,0 +1,15 @@
+# Declare providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"  # us-east-2, update as per your region
+  
+}
